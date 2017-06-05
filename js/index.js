ee8e0abc5903434aa4bc891da562a10886a0be6c
Ahilesh Gupta <iamakhileshgupta3@gmail.com>
2017-06-05 11:38:23 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var j = jQuery.noConflict();
+
+
+function load_thaneEvent() {
+	
+     document.getElementById("mainLoader").innerHTML='<object type="text/html" data="mainLoader.html" ></object>';
+}
+
+function load_thaneLoksatta() {
+     document.getElementById("mainLoader").innerHTML='<object type="text/html" data="http://www.loksatta.com/thane/" ></object>';
+}
+
+
+
+function load_hindi() {
+     document.getElementById("hindi").innerHTML='<object type="text/html" data="http://khabar.ndtv.com/" ></object>';
+}
+
+
+function load_guj() {
+     document.getElementById("mainLoader").innerHTML='<object type="text/html" data="http://www.gujaratsamachar.com/" ></object>';
+}
+
+function loadpage1(){
+	alert("234");
+			document.getElementById("mainLoader").innerHTML='<object type="text/html" data="http://allevents.in/thane" ></object>';	
+}
+
+
+function about(){
+	window.open("https://www.facebook.com/digital15hawk/?ref=bookmarks","_system");
+}
+
+function rateus(){
+
+	window.open("market://details?id=com.wTipsandTricks_3761762","_system");
+}
+
+function moreRelatedApps(){
+	window.open("https://play.google.com/store/search?q=digitalhawk2016&hl=en","_system");
+}
